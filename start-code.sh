8ee40f0a40f98941894323ec499a60fc7dcda4b2
mylzj <864716024@qq.com>
2019-10-09 11:59:03 +0800

    提交

--- start-code.sh
+++ start-code.sh
@@ -15,7 +15,7 @@ docker run --rm \
    -v $proj_home:/usr/src/mymaven \
    -w /usr/src/mymaven $img_mvn mvn clean package -U
 
-sudo mv /$proj_home/target/mobile-*.jar /$proj_home/target/demo.jar # 兼容所有sh脚本
+sudo mv /$PWD/target/mobile-*.jar /$PWD/target/demo.jar # 兼容所有sh脚本
 docker build -t $img_output .
 
 
